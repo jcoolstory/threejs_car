a7534f523373a6c40e3c625a1a0c974b4da68fb7
jcoolstory <jcoolstory@gmail.com>
2018-06-05 11:16:47 +0900

    phong material 적용

--- ex05/app.js
+++ ex05/app.js
@@ -4,6 +4,10 @@ var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHe
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+camera.position.z = 10;
+
 var manager = new THREE.LoadingManager();
 manager.onProgress = function ( item, loaded, total) {
     console.log( item, loaded, total);
@@ -18,27 +22,38 @@ var onProgress = function ( xhr) {
 
 var onError = function (xhr ) {
 };
-var texture = new THREE.TextureLoader(manager).load( 'res/skin01/0000.BMP' );
+
+var texture = new THREE.TextureLoader(manager).load( 'res/skin00/0000.BMP' );
 var loader = new THREE.OBJLoader(manager);
+var materialColor = new THREE.Color(1,1,1);
+var texturedMaterial =new THREE.MeshPhongMaterial( { color: materialColor, flatShading:false, side: THREE.DoubleSide } )
+//var texturedMaterial = new THREE.MeshPhongMaterial( { color: materialColor,wireframe: true , map: texture, side: THREE.DoubleSide } );
+//var texturedMaterial = new THREE.MeshPhongMaterial( { color: materialColor,wireframe: true } );
+texturedMaterial.shininess = 40;
+texturedMaterial.specular = new THREE.Color(1,1,1);
+texturedMaterial.map = texture;
+
 loader.load('res/Porsche_911_GT2.obj', function (obj){
     obj.traverse ( function ( child ) { 
         if ( child instanceof THREE.Mesh) {
-            child.material.map = texture;
+            
+            child.material = texturedMaterial;
         }
     });
 
     obj.position.y = 0;
+    obj.rotateX(1);
     scene.add( obj );
 }, onProgress, onError);
 
 var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
-scene.add( ambientLight );
 var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
 camera.add( pointLight );
-scene.add( camera );
+scene.add( ambientLight );
 
-var controls = new THREE.OrbitControls( camera, renderer.domElement );
-camera.position.z = 10;
+
+
+scene.add( camera );
 var light = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( light );
 var labelRX = document.getElementById("rotateX"); 
@@ -51,8 +66,8 @@ function toRadian(degree) {
 function animate(time) {
     requestAnimationFrame( animate );
     
-    labelRX.innerHTML = camera.rotation;
-    labelRY.innerHTML = camera.rotation;
+    labelRX.innerHTML = camera.rotation.x.toFixed(3);
+    labelRY.innerHTML = camera.rotation.y.toFixed(3);
 	renderer.render( scene, camera );
 }
 function StopWatch(dur, start, to){
